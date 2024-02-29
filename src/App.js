8aa864854ae44bb1b540fcf79caa13bc4bf4ccaa
Ulysse37 <ulysse.courtet@hotmail.fr>
2024-02-29 15:01:23 +0100

    ajout composant banner + passage en flex direction row sur main

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 
+
+import { USER_MAIN_DATA } from './assets/data.js';
+/* import { USER_ACTIVITY } from './assets/data.js';
+import { USER_AVERAGE_SESSIONS } from './assets/data.js';
+import { USER_PERFORMANCE } from './assets/data.js'; */
+
 import SideBar from "./components/SideBar/SideBar";
-/* import Banner from "./components/Banner/Banner";
-import KeyInfo from "./components/KeyInfo/KeyInfo"; */
+import Banner from "./components/Banner/Banner";
+/* import KeyInfo from "./components/KeyInfo/KeyInfo"; */
 
 function App() {
+  
   return (
     <main>
       <SideBar />
+      <Banner name={USER_MAIN_DATA[0].userInfos.firstName} text="Félicitation ! Vous avez explosé vos objectifs hier 👏" />
     </main>
   );
 }
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,1 +1,12 @@
 import './banner.css';
+
+function Banner({ name, text }) {
+  return (
+    <section className='banner-container'>
+      <h1>Bonjour <span>{name}</span></h1>
+      <p>{text}</p>
+    </section>
+  )
+}
+
+export default Banner;
