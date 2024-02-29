4fb53ac2c51d948a9457ac008d9b21406eabc7d2
Ulysse37 <ulysse.courtet@hotmail.fr>
2024-02-29 14:34:15 +0100

    ajout composant sidebar + boxshadow navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 
-/* import NavBar from "./components/NavBar/Navbar"; */
-/* import SideBar from "./components/SideBar/SideBar";
-import Banner from "./components/Banner/Banner";
+import SideBar from "./components/SideBar/SideBar";
+/* import Banner from "./components/Banner/Banner";
 import KeyInfo from "./components/KeyInfo/KeyInfo"; */
 
 function App() {
   return (
     <main>
-      
+      <SideBar />
     </main>
   );
 }
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,10 +1,39 @@
 import './sidebar.css';
+import YogaIcon from "../../assets/yoga-icon.svg";
+import SwimIcon from "../../assets/swimming-icon.svg";
+import CyclingIcon from "../../assets/bike-icon.svg";
+import DumbBellIcon from "../../assets/dumbbell-icon.svg";
 
 function SideBar() {
 
   return (
-    <nav>
-    </nav>
+    <aside className='sidebar-container'>
+      <nav className='sidebar'>
+        <ul>
+          <a href='/'>
+            <li>
+              <img src={YogaIcon} alt='Icône de yoga'/>
+            </li>
+          </a>
+          <a href='/'>
+            <li>
+              <img src={SwimIcon} alt='Icône de natation'/>
+            </li>
+          </a>
+          <a href='/'>
+            <li>
+              <img src={CyclingIcon} alt='Icône de cyclisme'/>
+            </li>
+          </a>
+          <a href='/'>
+            <li>
+              <img src={DumbBellIcon} alt="Icône d'haltère"/>
+            </li>
+          </a>
+        </ul>
+      </nav>
+      <p>Copiryght, SportSee 2020</p>
+    </aside>
   )
 }
 
