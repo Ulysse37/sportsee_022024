2bea5103109033e00436989fe1239dd96950dada
Ulysse37 <ulysse.courtet@hotmail.fr>
2024-03-01 11:57:28 +0100

    style linechart component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { USER_PERFORMANCE } from './assets/data.js'; */
 import SideBar from "./components/SideBar/SideBar";
 import Banner from "./components/Banner/Banner";
 import KeyInfoCard from "./components/KeyInfo/KeyInfoCard.jsx";
+import LineChartComponent from "./components/LineChart/LineChart";
 
 function App() {
   
@@ -18,6 +19,7 @@ function App() {
       <div className='central-container'>
         <Banner name={USER_MAIN_DATA[0].userInfos.firstName} text="Félicitation ! Vous avez explosé vos objectifs hier 👏" />
         <KeyInfoCard item={USER_MAIN_DATA[0]} />
+        <LineChartComponent />
       </div>
     </main>
   );
--- src/components/LineChart/LineChart.jsx
+++ src/components/LineChart/LineChart.jsx
@@ -1,1 +1,50 @@
 import './linechart.css';
+import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
+
+import { USER_AVERAGE_SESSIONS } from '../../assets/data';
+
+function ShortDayNameFormat(day) {
+  const days = ["L", "M", "M", "K", "V", "S", "D"];
+  return days[day - 1];
+}
+
+function LineChartComponent() {
+  const data = USER_AVERAGE_SESSIONS[0].sessions.map((session) => ({
+    day: ShortDayNameFormat(session.day),
+    sessionLength: session.sessionLength,
+  }));
+
+function CustomTooltip({ active, payload }) { //payload contient les valeurs pour le point où le curseur est positionné
+  if (active) {
+    return (
+      <div className="custom-tooltip-container">
+        <p className="custom-tooltip-text">{`${payload[0].value} min`}</p>
+      </div>
+    );
+  }
+
+  return null;
+}
+
+  return (
+    <div className="graph-container">
+      <ResponsiveContainer className="linechart-responsive-container" width="30%" height={263}>
+        <LineChart data={data}>
+          <XAxis dataKey="day" hide={true} />
+          <YAxis hide={true} />
+          <Line type="monotone" dataKey="sessionLength" stroke="#ffffff" dot={false} />
+          <Tooltip content={<CustomTooltip />} />
+        </LineChart>
+      </ResponsiveContainer>
+      <div className="day-labels"> 
+        {data.map((entry, index) => (
+          <span key={`day-label-${index}`} className="day-label">
+            {entry.day}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default LineChartComponent;
