7cd74d0954c0c3dc4651286705a80d99db7e0c23
Ulysse37 <ulysse.courtet@hotmail.fr>
2024-03-07 11:02:16 +0100

    finalisation axe x barchart

--- src/components/BarChart/BarChart.jsx
+++ src/components/BarChart/BarChart.jsx
@@ -9,14 +9,21 @@ function BarChartComponent({ data }) {
   const domainMax = maxKg + 2; // Valeur max à afficher sur l'axe Y avec 1 unité de marge
 
   return (
-    <ResponsiveContainer className="barchart-graph-container" width={835} height={320} >
-      <BarChart
-        data={data.sessions}
-      >
-        <CartesianGrid strokeDasharray="3" vertical={false}/>
-        <XAxis dataKey="day" />
+    <ResponsiveContainer className="barchart-graph-container" width={835} height={320}>
+      <BarChart data={data.sessions}>
+        <CartesianGrid strokeDasharray="3" vertical={false} />
+        <XAxis dataKey="day" tickFormatter={(value) => new Date(value).getDate()} /* formate les dates afin d'afficher les jours du mois en numéro */
+        tick={{ dy: 15 }}
+        tickLine={{ display: 'none' }}
+        scale='point' /* supprime l'espace à droite et à gauche de l'axe x */
+        padding={{ left: 13, right: 12 }} /* ajuste le positionnement des 1ères données à droite et à gauche de l'axe */
+        /> 
         <YAxis yAxisId="left" orientation="left" hide={true} />
-        <YAxis yAxisId="right" orientation="right" axisLine={false} domain={[domainMin, domainMax]} />
+        <YAxis yAxisId="right" orientation="right" axisLine={false} 
+        domain={[domainMin, domainMax]}
+        tickLine={{ display: 'none' }} 
+        tick={{ dx: 30 }}
+        />
         <Tooltip />
         <Legend />
         <Bar dataKey="kilogram" fill="black" yAxisId="right" barSize={10} radius={[4, 4, 0, 0]} />
