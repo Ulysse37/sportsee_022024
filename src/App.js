08b72b55ce3825fda392e4f472225e77beaabc08
Ulysse37 <ulysse.courtet@hotmail.fr>
2024-03-19 10:36:45 +0100

    4 fetch pour les 4 types de données de l'api + console log dans app

--- src/App.js
+++ src/App.js
@@ -12,6 +12,21 @@ import LineChartComponent from "./components/LineChart/LineChart";
 import RadarChartComponent from './components/RadarChart/RadarChart';
 import PieChartComponent from './components/PieChart/PieChart';
 
+import { getUserData } from './assets/services';
+import { getUserActivity } from './assets/services';
+import { getUserAverageSessions } from './assets/services';
+import { getUserPerformance } from './assets/services';
+
+const userId = 12;
+
+const userData = await getUserData(userId);
+const activityData = await getUserActivity(userId);
+const averageSessionsData = await getUserAverageSessions(userId);
+const performanceData = await getUserPerformance(userId);
+console.log("données utilisateur", userData);
+console.log("activité utilisateur", activityData);
+console.log("durée session utilisateur", averageSessionsData);
+console.log("performance utilisateur" ,performanceData);
 
 function App() {
   
--- src/assets/services.js
+++ src/assets/services.js
@@ -1,4 +1,61 @@
-function test() {
-  console.log('ayo');
-}
-test();
+const API_URL = 'http://localhost:3001'; // URL de l'api
+
+// Fetch pour récupérer les données utilisateur
+export const getUserData = async (userId) => {
+  try {
+    const response = await fetch(`${API_URL}/user/${userId}`);
+    if (!response.ok) {
+      throw new Error('Erreur lors de la récupération des données');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données :', error);
+    return null;
+  }
+};
+
+// Fetch pour récupérer les données d'activité utilisateur
+export const getUserActivity = async (userId) => {
+  try {
+    const response = await fetch(`${API_URL}/user/${userId}/activity`);
+    if (!response.ok) {
+      throw new Error('Erreur lors de la récupération des données');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données :', error);
+    return null;
+  }
+};
+
+// Fetch pour récupérer les moyennes de sessions utilisateur
+export const getUserAverageSessions = async (userId) => {
+  try {
+    const response = await fetch(`${API_URL}/user/${userId}/average-sessions`);
+    if (!response.ok) {
+      throw new Error('Erreur lors de la récupération des données');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données :', error);
+    return null;
+  }
+};
+
+// Fetch pour récupérer les performances utilisateur
+export const getUserPerformance = async (userId) => {
+  try {
+    const response = await fetch(`${API_URL}/user/${userId}/performance`);
+    if (!response.ok) {
+      throw new Error('Erreur lors de la récupération des données');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données :', error);
+    return null;
+  }
+};
