828a6eae67c3c79f04268f6cbc30eb48ff7b3041
Ulysse37 <ulysse.courtet@hotmail.fr>
2024-04-19 10:40:55 +0200

    Séparation formatage donnée dans fichier séparé

--- src/components/RadarChart/RadarChart.jsx
+++ src/components/RadarChart/RadarChart.jsx
@@ -1,20 +1,10 @@
 import './radarchart.css';
 import { Radar, RadarChart, PolarGrid, PolarAngleAxis, ResponsiveContainer } from 'recharts';
+import { formatRadarChartData } from '../../assets/dataFormating.js';
 
-const TypePerformance = { //contient les valeurs numériques à leurs mots correspondant
-  1: 'Cardio',
-  2: 'Energie',
-  3: 'Endurance',
-  4: 'Force',
-  5: 'Vitesse',
-  6: 'Intensité'
-};
    
 function RadarChartComponent({ perfData }) {
-  const userPerformance = perfData.data.map(item => ({
-    ...item, // créer copie de l'objet avec le type de performance en toute lettre
-    kind: TypePerformance[item.kind] //remplace la valeur par le type de performance correspondant à celle-ci
-  }))
+  const userPerformance = formatRadarChartData(perfData); // formate les types de performance en français avec une majuscule
 
   return (
     <ResponsiveContainer className="radarchart-responsive-container" width={258} height={274}>
